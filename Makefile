ac3787c0e067f211e7b3593dd027510fc4b7d305
Denis Merigoux <denis.merigoux@gmail.com>
2019-04-04 16:15:26 +0200

    Finished constant propagation + changed license to CeCiLL

--- Makefile
+++ Makefile
@@ -1,17 +1,33 @@
-# Copyright 2018 Denis Merigoux and INRIA
+# Copyright Inria, contributor: Denis Merigoux <denis.merigoux@inria.fr> (2018)
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This software is a computer program whose purpose is to compile and analyze
+# programs written in the M langage, created by thge DGFiP.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# This software is governed by the CeCILL-B license under French law and
+# abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL-B
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# http://www.cecill.info.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-B license and that you accept its terms.
 
 SOURCE_DIR=calculette-impots-m-source-code/sources-latin1/sourcesm2015m_4_6
 
--- src/cfg/union_find.ml
+++ src/cfg/union_find.ml
@@ -1,21 +1,36 @@
 (*
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Copyright Inria, contributor: Denis Merigoux <denis.merigoux@inria.fr> (2019)
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This software is a computer program whose purpose is to compile and analyze
+programs written in the M langage, created by thge DGFiP.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
+This software is governed by the CeCILL-B license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL-B
+license as circulated by CEA, CNRS and INRIA at the following URL
+http://www.cecill.info.
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-B license and that you accept its terms.
 *)
 
-
-(* A type with decidable equality, only requirement
-   for the union_find data structure *)
 module type EqType =
 sig
   type t
@@ -33,24 +48,16 @@ sig
   val union : t -> t -> unit
 end
 
-(* A functor with one argument to construct a union-find
-   structure. *)
 module Make (Eq: EqType) =
 struct
   type elt = Eq.t
-  (* A node of the disjoint set structure.
-     The rank and parent fields are mutable to enable path compression
-     and rank updates *)
   type t = Node of {
       mutable rank : int ;
       mutable parent : t ;
       elem : elt
     }
 
-  (* Creates a new union_find node, referencing
-     itself as its parent. *)
   let create elm =
-    (* `let rec ...` required here to reference ourselves *)
     let rec node = Node {
         rank = 0;
         parent = node;
@@ -58,11 +65,8 @@ struct
       }
     in node
 
-  (* Do a find() on a given node, searching
-     for its representative *)
   let rec find_aux (Node inst) =
     if inst.parent != (Node inst) then
-      (* Path-compression here *)
       let _ = inst.parent <- find_aux (inst.parent) in
       inst.parent
     else
@@ -74,8 +78,6 @@ struct
 
   let get_elt (Node inst) = inst.elem
 
-  (* Union function, performs union by rank and
-     uses find() for path compression *)
   let union node_a node_b =
     if node_a == node_b then ()
     else
