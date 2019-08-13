deb4322b14da285120cad5da5de275a8e96a2432
Raphaël Monat <raphael.monat@lip6.fr>
2019-08-13 17:16:12 +0200

    fix computations of IAVIM by setting FLAG_PVRO = 0

--- src/mlang/test_framework/tast.ml
+++ src/mlang/test_framework/tast.ml
@@ -32,7 +32,7 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-C license and that you accept its terms.
 *)
 
-module Pos = Specifisc.Pos
+module Pos = Verifisc.Pos
 
 type literal =
   | I of int
