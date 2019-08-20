66ddcd2747bcbaec6bfd2d146eaed3e47ccd09ec
Raphaël Monat <raphael.monat@lip6.fr>
2019-08-20 09:07:21 +0200

    Adds test2.m_spec

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,11 +2,26 @@
 
 # for i in $FILES; do
 #     echo -n "Testing $i...";
-#     TEST_FILE=$i make test 1>/dev/null;
+#     TEST_FILE=$i make test 1>/dev/null 2>/dev/null;
 #     if [ $? -eq 0 ]; then
 #         echo " OK"
 #     else
-#         echo " FAIL"
+#         TEST_FILE=$i make test2 1>/dev/null;
+#         if [ $? -eq 0 ]; then
+#             echo " OK2"
+#         else
+#             echo " FAIL"
+#         fi
 #     fi
 # done
-ls -rS tests/* | parallel 'TEST_FILE={} make test 1>/dev/null; if [ $? -eq 0 ]; then echo "Testing of {} successful"; else echo "Testing of {} failed"; fi'
+ls -rS tests/* | parallel 'TEST_FILE={} make test 1>/dev/null;
+if [ $? -eq 0 ]; then
+ echo "{}: success on tests.m_spec";
+else
+ TEST_FILE={} make test2 1>/dev/null;
+ if [ $? -eq 0 ]; then
+  echo "{}: success on test2.m_spec";
+ else
+   echo "{}: failure";
+ fi;
+fi'
