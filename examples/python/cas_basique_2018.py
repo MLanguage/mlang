43b524816afabc34137a65f1eb749399778430a5
Raphaël Monat <raphael.monat@lip6.fr>
2020-09-25 20:48:46 +0200

    Updated python based examples

--- examples/python/cas_basique_2018.py
+++
@@ -1,7 +0,0 @@
-import ir_2018
-
-print(ir_2018.extracted({
-    "1AJ": 10023,
-    "1BJ": 42598,
-    "0CF": 1
-}))
--- examples/python/cas_basique_2019.py
+++
@@ -1,7 +0,0 @@
-import ir_2019
-
-print(ir_2019.extracted({
-    "1AJ": 10023,
-    "1BJ": 42598,
-    "0CF": 1
-}))
