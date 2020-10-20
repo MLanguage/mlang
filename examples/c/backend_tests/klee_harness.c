77275e20cf8fc15d7cd9578f5b76b5600254e337
Denis Merigoux <denis.merigoux@gmail.com>
2020-10-21 00:39:46 +0200

    Move to KLEE

--- examples/c/backend_tests/klee_harness.c
+++ examples/c/backend_tests/klee_harness.c
@@ -1,34 +1,17 @@
-#include "ir_all_ins_no_outs_2018.h"
+#include "ir_simulateur_simplifie_inputs_no_outs_2018.h"
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int main()
 {
 
-    if (argc != 2)
-    {
-        printf("%d\n", argc);
-        return 1;
-    }
-
     int num_inputs = m_num_inputs();
-    long correct_string_size = 2 * num_inputs + 1;
-
-    FILE *input_file = fopen(argv[1], "r");
-    fseek(input_file, 0, SEEK_END);
-    long fsize = ftell(input_file);
-    if (fsize != correct_string_size)
-    {
-        printf("%d != %d\n", (int)correct_string_size, (int)fsize);
-        return 2;
-    }
-    rewind(input_file);
-    char input_string[correct_string_size];
-    fread(input_string, 1, fsize, input_file);
-    fclose(input_file);
+    int correct_string_size = 2 * num_inputs;
 
     // First we fill the array with the contents of the fuzzing input
     m_value input_array_for_m[num_inputs];
+    char input_string[correct_string_size];
+    klee_make_symbolic(&input_string, sizeof(input_string), "input_string");
 
     for (int i = 0; i < num_inputs; i++)
     {
@@ -48,10 +31,10 @@ int main(int argc, char *argv[])
     m_output output = m_extracted(input_for_m);
     if (output.is_error)
     {
-        return 3;
+        return 1;
     }
     else
     {
-        abort();
+        return 0;
     }
 }
