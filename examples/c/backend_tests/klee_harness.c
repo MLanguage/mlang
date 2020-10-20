dc01ed002da48c4beb26b76dc88f4a8338eb3353
Denis Merigoux <denis.merigoux@gmail.com>
2020-10-21 00:57:08 +0200

    Improve KLEE harness

--- examples/c/backend_tests/klee_harness.c
+++ examples/c/backend_tests/klee_harness.c
@@ -6,22 +6,19 @@ int main()
 {
 
     int num_inputs = m_num_inputs();
-    int correct_string_size = 2 * num_inputs;
 
     // First we fill the array with the contents of the fuzzing input
     m_value input_array_for_m[num_inputs];
-    char input_string[correct_string_size];
-    klee_make_symbolic(&input_string, sizeof(input_string), "input_string");
+    bool undefined_inputs[num_inputs];
+    double value_inputs[num_inputs];
+    klee_make_symbolic(&undefined_inputs, sizeof(undefined_inputs), "undefined_inputs");
+    klee_make_symbolic(&value_inputs, sizeof(value_inputs), "value_inputs");
 
     for (int i = 0; i < num_inputs; i++)
     {
-        // A char holds values from 0 to 65535 so it's a good range for
-        // our input values
-        char *value = input_string + 2 * i;
-        char *undefined = input_string + 2 * i + 1;
         m_value input = (struct m_value){
-            .undefined = (((unsigned int)*undefined) > 32767) ? true : false,
-            .value = (double)((unsigned int)(*value)),
+            .undefined = undefined_inputs[i],
+            .value = value_inputs[i],
         };
         input_array_for_m[i] = input;
     }
