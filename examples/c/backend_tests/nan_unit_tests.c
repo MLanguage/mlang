a47243931f8ec19d2463abb2d99e88840a506405
David MENTRÉ <david.mentre@bentobako.org>
2021-03-27 16:24:39 +0100

    Add more operators in unit tests

--- examples/c/backend_tests/nan_unit_tests.c
+++ examples/c/backend_tests/nan_unit_tests.c
@@ -8,7 +8,7 @@ m_value x_m_value[N] = {
   { 0.0, true}, // m_undefined
   { 0.0, true}, // m_undefined
   { 0.0, true}, // m_undefined
-  { 1.0, false },
+  { 1.6, false },
   { 0.0, false },
 };
 
@@ -24,7 +24,7 @@ m_nan_value x_m_nan_value[N] = {
   NAN, // m_nan_undefined
   NAN, // m_nan_undefined
   NAN, // m_nan_undefined
-  1.0,
+  1.6,
   0.0,
 };
 
@@ -44,7 +44,7 @@ int compare_nan_vs_m_value(const m_value v, const m_nan_value nan_v, int line)
   if (!v.undefined && !M_NAN_UNDEFINED(nan_v) && v.value == nan_v)
     return 0;
 
-  printf("ERROR: line:%d m_value (%f,%s) != m_nan_value(%f)\n",
+  printf("ERROR: line:%d m_value(%f,%s) != m_nan_value(%f)\n",
          line, v.value, v.undefined ? "UNDEF" : "DEF", nan_v);
 
   return 1;
@@ -98,6 +98,29 @@ int compare_operators(void)
   err += compare_binary_op(m_add, m_nan_add, __LINE__);
   err += compare_binary_op(m_sub, m_nan_sub, __LINE__);
   err += compare_unary_op(m_neg, m_nan_neg, __LINE__);
+  err += compare_binary_op(m_mul, m_nan_mul, __LINE__);
+  err += compare_binary_op(m_div, m_nan_div, __LINE__); // TODO: error!
+  err += compare_binary_op(m_lt, m_nan_lt, __LINE__);
+  err += compare_binary_op(m_lte, m_nan_lte, __LINE__);
+  err += compare_binary_op(m_gt, m_nan_gt, __LINE__);
+  err += compare_binary_op(m_gte, m_nan_gte, __LINE__);
+  err += compare_binary_op(m_eq, m_nan_eq, __LINE__);
+  err += compare_binary_op(m_neq, m_nan_neq, __LINE__);
+  err += compare_binary_op(m_and, m_nan_and, __LINE__);
+  err += compare_binary_op(m_or, m_nan_or, __LINE__);
+  err += compare_unary_op(m_not, m_nan_not, __LINE__); // TODO: errors!
+  // TODO: m_cond to test
+  err += compare_binary_op(m_min, m_nan_min, __LINE__);
+  err += compare_binary_op(m_max, m_nan_max, __LINE__);
+  err += compare_unary_op(m_present, m_nan_present, __LINE__);
+  err += compare_unary_op(m_null, m_nan_null, __LINE__);
+  err += compare_unary_op(m_round, m_nan_round, __LINE__);
+  err += compare_unary_op(m_floor, m_nan_floor, __LINE__);
+  // TODO: err += compare_unary_op(m_is_defined_true, m_nan_is_defined_true, __LINE__);
+  // TODO: m_is_defined_false
+  // TODO: m_nan_literal
+  // TODO: m_array_index
+  // TODO: m_multimax
 
   return err;
 }
