824bb0b06368ef2a847f5d299e3152fd193211b0
jemsab <5462724+jemsab@users.noreply.github.com>
2021-08-11 18:53:46 +0200

    m_error struct in c

--- examples/dgfip_c/m_error.h
+++ examples/dgfip_c/m_error.h
@@ -8,7 +8,11 @@
 typedef enum error_kind { Anomaly, Discordance, Information } error_kind;
 
 typedef struct m_error {
-  error_kind kind;
+  char kind[2];
+  char major_code[4];
+  char minor_code[7];
+  char description[81];
+  char isisf[2];
 } m_error;
 
 #endif /* M_ERROR_ */
