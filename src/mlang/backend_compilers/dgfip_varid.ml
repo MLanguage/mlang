09a01b04ab7b7fd57a865b9cfd0f9eb56e2a01fe
David Declerck <david.declerck@ocamlpro.com>
2021-09-30 15:05:19 +0200

    [DGFiP] Change computed variable ID map to a regular VariableMap

--- src/mlang/backend_compilers/dgfip_varid.ml
+++ src/mlang/backend_compilers/dgfip_varid.ml
@@ -11,23 +11,20 @@
    You should have received a copy of the GNU General Public License along with this program. If
    not, see <https://www.gnu.org/licenses/>. *)
 
-module StringMap = Map.Make (String)
+type var_id = VarInput of int | VarBase of int | VarComputed of int
 
-type var_id =
- | VarInput of int
- | VarBase of int
- | VarComputed of int
-
-type var_id_map = var_id StringMap.t
+type var_id_map = var_id Mir.VariableMap.t
 
 let gen_access_def vm v =
-  match StringMap.find v vm with
-  | VarInput i -> Printf.sprintf "DS_[%d /*%s*/]" i v
-  | VarBase i -> Printf.sprintf "DB_[%d /*%s*/]" i v
-  | VarComputed i -> Printf.sprintf "DC_[%d /*%s*/]" i v
+  let vn = Pos.unmark v.Mir.Variable.name in
+  match Mir.VariableMap.find v vm with
+  | VarInput i -> Printf.sprintf "DS_[%d /*%s*/]" i vn
+  | VarBase i -> Printf.sprintf "DB_[%d /*%s*/]" i vn
+  | VarComputed i -> Printf.sprintf "DC_[%d /*%s*/]" i vn
 
 let gen_access_val vm v =
-  match StringMap.find v vm with
-  | VarInput i -> Printf.sprintf "S_[%d /*%s*/]" i v
-  | VarBase i -> Printf.sprintf "B_[%d /*%s*/]" i v
-  | VarComputed i -> Printf.sprintf "C_[%d /*%s*/]" i v
+  let vn = Pos.unmark v.Mir.Variable.name in
+  match Mir.VariableMap.find v vm with
+  | VarInput i -> Printf.sprintf "S_[%d /*%s*/]" i vn
+  | VarBase i -> Printf.sprintf "B_[%d /*%s*/]" i vn
+  | VarComputed i -> Printf.sprintf "C_[%d /*%s*/]" i vn
