815a323789454531f8418cb32f5dc51404f22b89
David Declerck <david.declerck@ocamlpro.com>
2021-09-30 10:46:54 +0200

    [DGFiP] Provide computed variable IDs to code generation function

--- src/mlang/backend_compilers/bir_to_dgfip_c.ml
+++ src/mlang/backend_compilers/bir_to_dgfip_c.ml
@@ -682,8 +682,12 @@ let generate_implem_header oc header_filename =
   Format.fprintf oc "#include \"%s\"\n\n" header_filename;
   Format.fprintf oc "#include <string.h>\n"
 
+let generate_cond_table _ v error_set =
+  List.fold_left (fun acc err -> ErrorSet.add err acc) error_set v.cond_errors
+
 let generate_c_program (program : Bir.program)
-    (function_spec : Bir_interface.bir_function) (filename : string) : unit =
+    (function_spec : Bir_interface.bir_function) (filename : string)
+    (_vm: Dgfip_varid.var_id_map) (_vma: Dgfip_varid.var_id_map) : unit =
   if Filename.extension filename <> ".c" then
     Errors.raise_error
       (Format.asprintf "Output file should have a .c extension (currently %s)"
--- src/mlang/driver.ml
+++ src/mlang/driver.ml
@@ -216,9 +216,10 @@ let driver (files : string list) (debug : bool) (var_info_debug : string list)
             Cli.debug_print "Compiling the codebase to DGFiP C...";
             if !Cli.output_file = "" then
               Errors.raise_error "an output file must be defined with --output";
-            Dgfip_gen_files.generate_auxiliary_files source_m_program;
+            let vm, vma = Dgfip_gen_files.generate_auxiliary_files
+                source_m_program in
             Bir_to_dgfip_c.generate_c_program combined_program function_spec
-              !Cli.output_file;
+              !Cli.output_file vm vma;
             Cli.debug_print "Result written to %s" !Cli.output_file
           end
           else
