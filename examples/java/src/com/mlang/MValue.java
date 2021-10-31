b17ff0683f2295a38d1499d81dbe363a78bcb264
James Barnes <5462724+jemsab@users.noreply.github.com>
2021-10-31 12:09:49 +0100

    Formatting and cleanup

--- examples/java/src/com/mlang/MValue.java
+++ examples/java/src/com/mlang/MValue.java
@@ -1,10 +1,8 @@
 package com.mlang;
 
 import java.util.List;
-import java.util.function.BiFunction;
 
 public class MValue {
-
 
   public static final MValue mUndefined = new MValue(0., true);
   public static final MValue zero = new MValue(0., false);
@@ -13,111 +11,111 @@ public class MValue {
   private final double value;
   private final boolean undefined;
 
-   public MValue(double value, boolean isDefined){
-     this.value = value;
-     this.undefined = isDefined;
+  public MValue(double value, boolean isDefined) {
+    this.value = value;
+    this.undefined = isDefined;
   }
 
-public MValue(double value){
-     this.value = value;
-     this.undefined = false;
+  public MValue(double value) {
+    this.value = value;
+    this.undefined = false;
   }
 
   public double getValue() {
     return this.value;
-  } 
+  }
 
   public boolean isUndefined() {
     return this.undefined;
   }
 
-  private static MValue boolToMValue(boolean b){
-   return b ? one : zero;
+  private static MValue boolToMValue(boolean b) {
+    return b ? one : zero;
   }
 
   public static MValue mGreaterThan(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
-    return boolToMValue(x.getValue() > y.getValue()); 
+    }
+    return boolToMValue(x.getValue() > y.getValue());
   }
 
   public static MValue mGreaterThanEqual(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
-   return boolToMValue(x.getValue() >= y.getValue()); 
+    }
+    return boolToMValue(x.getValue() >= y.getValue());
   }
 
   public static MValue mLessThan(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() < y.getValue());
   }
 
   public static MValue mLessThanEqual(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() <= y.getValue());
   }
 
   public static MValue mEqual(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() == y.getValue());
   }
 
   public static MValue mNotEqual(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() != y.getValue());
   }
 
   public static MValue mAnd(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined())  {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() != 0 && y.getValue() != 0);
   }
 
   public static MValue mOr(MValue x, MValue y) {
-    if(x.isUndefined() && y.isUndefined())  {
+    if (x.isUndefined() && y.isUndefined()) {
       return mUndefined;
-    } 
+    }
     return boolToMValue(x.getValue() != 0 || y.getValue() != 0);
   }
 
   public static MValue mAdd(MValue x, MValue y) {
 
-    if(x.isUndefined() && y.isUndefined())  {
+    if (x.isUndefined() && y.isUndefined()) {
       return mUndefined;
-    } 
-    
+    }
+
     return new MValue(x.getValue() + y.getValue());
   }
 
   public static MValue mSubtract(MValue x, MValue y) {
-    if(x.isUndefined() && y.isUndefined())  {
+    if (x.isUndefined() && y.isUndefined()) {
       return mUndefined;
-    } 
+    }
 
     return new MValue(x.getValue() - y.getValue());
   }
 
   public static MValue mMultiply(MValue x, MValue y) {
-    if(x.isUndefined() || y.isUndefined()) {
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
     }
     return new MValue(x.getValue() * y.getValue());
   }
 
   public static MValue mDivide(MValue x, MValue y) {
-   
-    if(x.isUndefined() || y.isUndefined()) {
+
+    if (x.isUndefined() || y.isUndefined()) {
       return mUndefined;
     }
 
@@ -130,21 +128,20 @@ public MValue(double value){
     return new MValue(x.getValue() / denominateur);
   }
 
-
   public static MValue m_round(MValue x) {
     if (x.isUndefined()) {
       return mUndefined;
     }
     double dValue = x.getValue();
     double valueToRound = dValue + (dValue < 0 ? -0.50005 : 0.50005);
-    return new MValue((double)(int)(valueToRound));
+    return new MValue((double) (int) (valueToRound));
   }
 
   public static MValue m_floor(MValue x) {
     if (x.isUndefined()) {
       return mUndefined;
     }
-    double valueToFloor = x.getValue()+ 0.000001;
+    double valueToFloor = x.getValue() + 0.000001;
     return new MValue(Math.floor(valueToFloor));
   }
 
@@ -162,23 +159,23 @@ public MValue(double value){
     return new MValue(Math.max(x.getValue(), y.getValue()));
   }
 
-  public static MValue m_min(MValue x, MValue y) { 
+  public static MValue m_min(MValue x, MValue y) {
     return new MValue(Math.min(x.getValue(), y.getValue()));
   }
 
   public static MValue mNeg(MValue x) {
-    if(x.isUndefined()) {
+    if (x.isUndefined()) {
       return mUndefined;
     }
     return new MValue(-x.getValue());
   }
 
   public static MValue mPresent(MValue value) {
-   return value.isUndefined() ? zero : one;
+    return value.isUndefined() ? zero : one;
   }
 
   public static MValue mNot(MValue value) {
-    if(value.isUndefined()) {
+    if (value.isUndefined()) {
       return mUndefined;
     }
     return value.getValue() == 0 ? one : zero;
@@ -188,7 +185,7 @@ public MValue(double value){
     if (bound.isUndefined()) {
       throw new RuntimeException("Multimax bound undefined!");
     } else {
-      int max_index = (int)Math.floor(bound.getValue());
+      int max_index = (int) Math.floor(bound.getValue());
       MValue max = mAdd(m_array_index(array, zero), zero);
       for (int i = 0; i <= max_index; i++) {
         MValue challenger = mAdd(m_array_index(array, new MValue(i)), zero);
@@ -216,40 +213,32 @@ public MValue(double value){
     }
   }
 
-  public static MValue m_array_index(List<MValue> array, MValue index)
-{
-    if (index.isUndefined())
-    {
+  public static MValue m_array_index(List<MValue> array, MValue index) {
+    if (index.isUndefined()) {
+      return mUndefined;
+    } else {
+      if (index.getValue() < 0) {
+        return zero;
+      } else if (index.getValue() >= array.size() - 1) {
         return mUndefined;
+      } else {
+        return array.get((int) (index.getValue()));
+      }
     }
-    else
-    {
-        if (index.getValue() < 0)
-        {
-            return zero;
-        }
-        else if (index.getValue() >= array.size() - 1)
-        {
-            return mUndefined;
-        }
-        else
-        {
-            return array.get((int)(index.getValue()));
-        }
-    }
-}
+  }
 
   public boolean equalsWithThreshold(MValue toCompare, double withThreshold) {
-    return Math.abs(this.getValue() - toCompare.getValue()) < withThreshold; 
+    return Math.abs(this.getValue() - toCompare.getValue()) < withThreshold;
   }
 
   @Override
   public boolean equals(Object o) {
-    return o != null && o instanceof MValue && ((MValue)o).getValue() == this.getValue() && ((MValue)o).isUndefined() == this.isUndefined();
+    return o != null && o instanceof MValue && ((MValue) o).getValue() == this.getValue()
+        && ((MValue) o).isUndefined() == this.isUndefined();
   }
 
   @Override
   public String toString() {
-    return "Value: " + this.getValue() + " undefined: " + this.isUndefined(); 
+    return "Value: " + this.getValue() + " undefined: " + this.isUndefined();
   }
 }
