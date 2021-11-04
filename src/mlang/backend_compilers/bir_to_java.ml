011c640d0b4b65671d8107451ac96700ce1412d0
James Barnes <5462724+jemsab@users.noreply.github.com>
2021-11-04 15:24:24 +0100

    add copyright and change contributor info

--- src/mlang/backend_compilers/bir_to_java.ml
+++ src/mlang/backend_compilers/bir_to_java.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2019 Inria, contributor: Denis Merigoux <denis.merigoux@inria.fr>
+(* Copyright (C) 2021 Inria, contributor: James Barnes <bureau.si-part-ircalcul@dgfip.finances.gouv.fr> 
 
    This program is free software: you can redistribute it and/or modify it under the terms of the
    GNU General Public License as published by the Free Software Foundation, either version 3 of the
