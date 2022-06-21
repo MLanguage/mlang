428ccf1a98146228603d9204f3924d2a24df0e38
Mathieu Durero <mathieu.durero@dgfip.finances.gouv.fr>
2022-06-21 16:41:42 +0200

    Test harness for OCaml backend: can print discrepancy reports for a single FIP file or all files from a test directory.

--- examples/ocaml/test_harness.ml
+++ examples/ocaml/test_harness.ml
@@ -1,19 +1,8 @@
-open
-  Mvalue
-  
-  (* .Chargement d'un fichier FIP 
-  .Sortir la structure revenue_code list 
-  .Appeler Ir_xx.calculate_tax
-  Sélectionner la calculatrice à l'exécution et pas à la compilation
-  .Récupérer les valeurs de sorties 
-  Comparer les valeurs de sorties à celles attendues précisées dans le fichier FIP 
-  .Écrire le résultat quelque part *)[@ocamlformat "disable"]
+open Mvalue
 
 type test_block = { block_name : string; block_data : Mvalue.revenue_code list }
 
 type test_data = { test_name : string; blocks : test_block list }
-
-let compare_rev_code code1 code2 : int = compare code1.alias code2.alias
 
 let load_file (input_file : string) : string list =
   let ic = open_in input_file in
@@ -28,6 +17,22 @@ let load_file (input_file : string) : string list =
         raise e
   in
   build_list []
+
+let get_file_in_dir (dir_handle : Unix.dir_handle) : string list =
+  let rec build_list file_list =
+    match Unix.readdir dir_handle with
+    | next_file when next_file = "." || next_file = ".." -> build_list file_list
+    | next_file -> build_list (next_file :: file_list)
+    | exception End_of_file ->
+        Unix.closedir dir_handle;
+        file_list
+    | exception e ->
+        Unix.closedir dir_handle;
+        raise e
+  in
+  build_list []
+
+let compare_rev_code code1 code2 : int = compare code1.alias code2.alias
 
 let line2revenue_code line : Mvalue.revenue_code =
   match String.split_on_char '/' line with
@@ -74,14 +79,14 @@ let entry_list (input_file : string) : Mvalue.revenue_code list =
 let reference_list (input_file : string) : Mvalue.revenue_code list =
   get_data_block input_file "#RESULTATS-PRIMITIF"
 
-let filter_output out_list ref_list : Mvalue.revenue_code list =
+let filter_rev_code_list initial_list ref_list : Mvalue.revenue_code list =
   let alias_eq code1 code2 =
     if code1.alias = code2.alias then true else false
   in
   let ref_exists code : bool =
     List.exists (fun ref_code -> alias_eq code ref_code) ref_list
   in
-  List.filter ref_exists out_list
+  List.filter ref_exists initial_list
 
 let list_discrepancies filtered_list ref_list :
     (Mvalue.revenue_code * Mvalue.revenue_code) list =
@@ -93,51 +98,84 @@ let list_discrepancies filtered_list ref_list :
        (List.sort compare_rev_code filtered_list)
        (List.sort compare_rev_code ref_list))
 
-let write_codes (_output_file : string) (result_list : Mvalue.revenue_code list)
-    : unit =
-  let _oc = open_out _output_file in
-  let oc = Format.formatter_of_out_channel _oc in
-  let print_result_line fmt (result : Mvalue.revenue_code) =
-    Format.fprintf fmt "%s : %f" result.alias result.value
-  in
-  let print_results fmt (result_list : Mvalue.revenue_code list) =
-    Format.pp_print_list print_result_line fmt result_list
-  in
-  Format.fprintf oc "@[<v 0>%a@]@." print_results result_list;
-  close_out _oc
+let print_rev_code (oc : Format.formatter) (rev_code : Mvalue.revenue_code) :
+    unit =
+  Format.fprintf oc "%s/%f" rev_code.alias rev_code.value
 
-let write_discrepancies (_output_file : string)
-    (discrepancy_list : (Mvalue.revenue_code * Mvalue.revenue_code) list) : unit =
-  let _oc = open_out _output_file in
-  let oc = Format.formatter_of_out_channel _oc in
+let compute_discrepancies_from_file_2020 (fip_file : string) :
+    (Mvalue.revenue_code * Mvalue.revenue_code) list =
+  let tax_result = Ir_tests_2020.calculate_tax (entry_list fip_file) in
+  let ref_list = reference_list fip_file in
+  Format.printf "Test case: %s@." fip_file;
+  list_discrepancies (filter_rev_code_list tax_result ref_list) ref_list
+
+let print_file_discrepancies (oc : Format.formatter)
+    (file_discrepancies_list :
+      string * (Mvalue.revenue_code * Mvalue.revenue_code) list) : unit =
   let print_line fmt (discrepancy : Mvalue.revenue_code * Mvalue.revenue_code) =
     let result, reference = discrepancy in
-    Format.fprintf fmt "%s/%f  %s/%f" result.alias result.value reference.alias
-      reference.value
+    Format.fprintf fmt "%a instead of %a" print_rev_code result print_rev_code
+      reference
   in
   let print_results fmt
       (result_list : (Mvalue.revenue_code * Mvalue.revenue_code) list) =
     Format.pp_print_list print_line fmt result_list
   in
-  Format.fprintf oc "@[<v 0>Valeur calculée | Référence@,%a@]@." print_results
-    discrepancy_list;
+  let filename, discrepancy_list = file_discrepancies_list in
+  Format.fprintf oc "@[<v 2>File: %s@,%a@]@." filename print_results
+    discrepancy_list
+
+let print_discrepancies (oc : Format.formatter)
+    (discrepancy_list : (Mvalue.revenue_code * Mvalue.revenue_code) list)
+    (fip_file : string) : unit =
+  let print_if_empty fmt (file, _) =
+    Format.fprintf fmt "No discrepancy for file %s" file
+  in
+  Format.fprintf oc
+    "@[<v 0>OCaml computed value | Reference value from file@,%a@]@."
+    (if List.length discrepancy_list <> 0 then print_file_discrepancies
+    else print_if_empty)
+    (fip_file, discrepancy_list)
+
+let test_FIP_2020 (fip_file : string) (output_file_name : string) : unit =
+  Format.printf "Simple test on file %s.@." fip_file;
+  let _oc = open_out (output_file_name ^ "_disc.txt") in
+  let oc = Format.formatter_of_out_channel _oc in
+  let discrepancy_list = compute_discrepancies_from_file_2020 fip_file in
+  print_discrepancies oc discrepancy_list fip_file;
+  close_out _oc
+
+let run_test_directory (directory : string) (output_file_name : string) : unit =
+  let dir_handle = Unix.opendir directory in
+  let file_list =
+    List.map (String.cat directory) (get_file_in_dir dir_handle)
+  in
+  let files_discrepancies_list =
+    List.combine file_list
+      (List.map compute_discrepancies_from_file_2020 file_list)
+  in
+  let _oc = open_out (output_file_name ^ "_disc.txt") in
+  let oc = Format.formatter_of_out_channel _oc in
+  let print_discrepancy_report fmt files_discrepancies_list =
+    Format.pp_print_list print_file_discrepancies fmt files_discrepancies_list
+  in
+  let result_to_print =
+    List.filter
+      (fun (filename, discrepancy_list) -> List.length discrepancy_list <> 0)
+      files_discrepancies_list
+  in
+  let print_if_empty fmt _placeholder =
+    Format.fprintf fmt "Nothing to see here…"
+  in
+  Format.fprintf oc "@[<v 0>Discrepancy report@,%a@]@."
+    (if List.length result_to_print <> 0 then print_discrepancy_report
+    else print_if_empty)
+    result_to_print;
   close_out _oc
 
 let () =
-  let tax_result = Ir_tests_2020.calculate_tax (entry_list Sys.argv.(1)) in
-  let ref_list = reference_list Sys.argv.(1) in
-  Format.printf "Programme %s lancé.\nA lu dans %s\n Écrira x_%s\n" Sys.argv.(0)
-    Sys.argv.(1) Sys.argv.(2);
-  write_codes ("Aentprim_" ^ Sys.argv.(2)) (entry_list Sys.argv.(1));
-  write_codes ("Aresprim_" ^ Sys.argv.(2)) (List.sort compare_rev_code ref_list);
-  write_codes
-    ("Arescorr_" ^ Sys.argv.(2))
-    (get_data_block Sys.argv.(1) "#RESULTATS-CORRECTIF");
-  write_codes
-    ("Acalcul_" ^ Sys.argv.(2))
-    (List.sort compare_rev_code tax_result);
-  write_discrepancies
-    ("Adisc_" ^ Sys.argv.(2))
-    (list_discrepancies (filter_output tax_result ref_list) ref_list)
-
-(* (Ir_basic_case.calculate_tax entry_list)*)
+  Format.printf "Starting %s.@." Sys.argv.(0);
+  match Sys.argv.(1) with
+  | "mono" -> test_FIP_2020 Sys.argv.(2) Sys.argv.(3)
+  | "multi" -> run_test_directory Sys.argv.(2) Sys.argv.(3)
+  | other -> Format.printf "Unknown command: %s@." other
