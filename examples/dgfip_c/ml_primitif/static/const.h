9dd9fb788e5af115bd02b2931ae4b11bab980831
Mathieu Durero <mathieu.durero@dgfip.finances.gouv.fr>
2022-12-27 16:52:21 +0100

    Replace shell script by makefile

---
+++ examples/dgfip_c/ml_primitif/static/const.h
@@ -0,0 +1,127 @@
+
+#ifndef _CONST_H_
+#define _CONST_H_
+
+#include <setjmp.h>
+
+#include "conf.h"
+
+#ifdef FLG_COMPACT
+
+struct S_irdata
+{
+  double valeurs[NB_VARS];
+  char defs[NB_VARS];
+};
+
+#define S_ irdata->valeurs
+#define C_ irdata->valeurs
+#define B_ irdata->valeurs
+#define DS_ irdata->defs
+#define DC_ irdata->defs
+#define DB_ irdata->defs
+
+#else
+
+struct S_irdata
+{
+  double *saisie;
+  double *calculee;
+  double *base;
+  char *def_saisie;
+  char *def_calculee;
+  char *def_base;
+#ifdef FLG_MULTITHREAD
+  T_discord *discords;
+  T_discord *tas_discord;
+  T_discord **p_discord;
+  int nb_bloquantes;
+  int max_bloquantes;
+  jmp_buf jmp_bloq;
+#endif /* FLG_MULTITHREAD */
+};
+
+#define S_ irdata->saisie
+#define C_ irdata->calculee
+#define B_ irdata->base
+#define DS_ irdata->def_saisie
+#define DC_ irdata->def_calculee
+#define DB_ irdata->def_base
+
+#define EST_SAISIE    0x0000
+#define EST_CALCULEE  0x4000
+#define EST_BASE      0x8000
+#define EST_MASQUE    0xc000
+#define INDICE_VAL    0x3fff
+
+#endif /* FLG_COMPACT */
+
+#define RESTITUEE    5
+#define RESTITUEE_P  6
+#define RESTITUEE_C  7
+
+struct S_erreur
+{
+  char *message;
+  char *codebo;
+  char *souscode;
+  char *isisf;
+  char *nom;
+  short type;
+};
+
+struct S_discord
+{
+  struct S_discord *suivant;
+  T_erreur *erreur;
+};
+
+#ifdef FLG_MULTITHREAD
+
+extern void add_erreur(T_irdata *irdata, T_erreur *erreur, char *code);
+extern void free_erreur();
+
+extern double my_ceil(double); /* ceil(a - 0.000001); */
+extern double my_floor(double); /* floor(a + 0.000001); */
+extern double my_arr(double); /* floor(v1 + v2 + 0.5) */
+
+#else
+
+extern void add_erreur(T_erreur *erreur, char *code);
+extern void free_erreur();
+
+#define my_ceil(a)	(ceil((a) - 0.000001))
+
+#ifdef FLG_OPTIM_MIN_MAX
+
+#define my_floor(a)	(floor_g((a) + 0.000001))
+/*#define my_arr(a)	(floor_g((a) + 0.50005)) *//* Ancienne version (2021) */
+#define my_arr(a)	(((a) < 0.0) ? ceil_g((a) - .50005) : floor_g((a) + .50005))
+
+#else
+
+#define my_floor(a)	(floor((a) + 0.000001))
+#define my_arr(a)	((double)(long long)(((a) < 0.0) ? ((a) - .50005) : ((a) + .50005)))
+
+#endif /* FLG_OPTIM_MIN_MAX */
+
+#endif /* FLG_MULTITHREAD */
+
+#define min(a,b)	(((a) <= (b)) ? (a) : (b))
+#define max(a,b)	(((a) >= (b)) ? (a) : (b))
+#define divd(a,b)	(((b) != 0.0) ? (a / b) : 0.0)
+
+#ifdef FLG_OPTIM_MIN_MAX
+
+#define fabs(a)		(((a) < 0.0) ? -(a) : (a))
+
+#endif /* FLG_OPTIM_MIN_MAX */
+
+extern double floor_g(double);
+extern double ceil_g(double);
+extern int multimax_def(int, char *);
+extern double multimax(double, double *);
+extern int modulo_def(int, int);
+extern double modulo(double, double);
+
+#endif /* _CONST_H_ */
---
+++ examples/dgfip_c/ml_primitif/static/irdata.c
@@ -0,0 +1,327 @@
+
+#include "conf.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "var.h"
+
+static bool alloc_tab(double **tab, char **def_tab, int taille)
+{
+  if ((tab == NULL) || (def_tab == NULL)) {
+    return false;
+  }
+  if (taille > 0) {
+    *tab = (double *)malloc(taille * sizeof(double));
+    if (*tab == NULL) {
+      return false;
+    }
+    *def_tab = (char *)malloc(taille * sizeof(char));
+    if (*def_tab == NULL) {
+      free(*tab);
+      *tab = NULL;
+      return false;
+    }
+  }
+  return true;
+}
+
+T_irdata * IRDATA_new_irdata(void)
+{
+  T_irdata *irdata = NULL;
+  if ((irdata = (T_irdata *)malloc(sizeof(T_irdata))) == NULL) {
+    return NULL;
+  }
+#ifndef FLG_COMPACT
+  irdata->saisie = NULL;
+  irdata->def_saisie = NULL;
+  irdata->calculee = NULL;
+  irdata->def_calculee = NULL;
+  irdata->base = NULL;
+  irdata->def_base = NULL;
+  if (alloc_tab(&irdata->saisie, &irdata->def_saisie, TAILLE_SAISIE) == false) {
+    IRDATA_delete_irdata(irdata);
+    return NULL;
+  }
+  if (alloc_tab(&irdata->calculee, &irdata->def_calculee, TAILLE_CALCULEE) == false) {
+    IRDATA_delete_irdata(irdata);
+    return NULL;
+  }
+  if (alloc_tab(&irdata->base, &irdata->def_base, TAILLE_BASE) == false) {
+    IRDATA_delete_irdata(irdata);
+    return NULL;
+  }
+#ifdef FLG_MULTITHREAD
+  irdata->discords = NULL;
+  irdata->tas_discord = NULL;
+  irdata->p_discord = &irdata->discords;
+  irdata->nb_bloquantes = 0;
+  irdata->max_bloquantes = 0;
+#endif /* FLG_MULTITHREAD */
+#endif /* !FLG_COMPACT */
+  IRDATA_reset_irdata(irdata);
+  return irdata;
+}
+
+void IRDATA_delete_irdata(T_irdata *irdata)
+{
+  if (irdata != NULL) {
+#ifndef FLG_COMPACT
+    if (irdata->saisie != NULL) free(irdata->saisie);
+    if (irdata->calculee != NULL) free(irdata->calculee);
+    if (irdata->base != NULL) free(irdata->base);
+    if (irdata->def_saisie != NULL) free(irdata->def_saisie);
+    if (irdata->def_calculee != NULL) free(irdata->def_calculee);
+    if (irdata->def_base != NULL) free(irdata->def_base);
+#endif /* FLG_COMPACT */
+#ifdef FLG_MULTITHREAD
+    IRDATA_reset_erreur(irdata);
+    while (irdata->tas_discord != NULL) {
+      *(irdata->p_discord) = irdata->tas_discord;
+      irdata->tas_discord = (irdata->tas_discord)->suivant;
+      free(*irdata->p_discord);
+    }
+#endif /* FLG_MULTITHREAD */
+    free(irdata);
+  }
+}
+
+static void reset_tab(double *p_double, char *p_char, int nb)
+{
+  memset(p_double, 0, sizeof(double) * nb);
+  memset(p_char, 0, nb);
+}
+
+void IRDATA_reset_irdata(T_irdata *irdata)
+{
+#ifdef FLG_COMPACT
+  reset_tab(irdata->valeurs, irdata->defs, TAILLE_TOTALE);
+#else
+  reset_tab(irdata->saisie, irdata->def_saisie, TAILLE_SAISIE);
+  reset_tab(irdata->calculee, irdata->def_calculee, TAILLE_CALCULEE);
+  reset_tab(irdata->base, irdata->def_base, TAILLE_BASE);
+#endif /* FLG_COMPACT */
+#ifdef FLG_MULTITHREAD
+  IRDATA_reset_erreur(irdata);
+#endif /* FLG_MULTITHREAD */
+}
+
+void IRDATA_reset_base(T_irdata *irdata)
+{
+#ifdef FLG_COMPACT
+  reset_tab(irdata->valeurs + TAILLE_SAISIE + TAILLE_CALCULEE, irdata->defs + TAILLE_SAISIE + TAILLE_CALCULEE, TAILLE_BASE);
+#else
+  reset_tab(irdata->base, irdata->def_base, TAILLE_BASE);
+#endif /* FLG_COMPACT */
+}
+
+void IRDATA_reset_light(irdata)
+T_irdata *irdata ;
+{
+reset_tab(irdata->saisie, irdata->def_saisie, TAILLE_SAISIE) ;
+reset_tab(irdata->calculee, irdata->def_calculee, TAILLE_CALCULEE) ;
+}
+
+void IRDATA_reset_calculee(T_irdata *irdata)
+{
+#ifdef FLG_COMPACT
+  reset_tab(irdata->valeurs + TAILLE_SAISIE, irdata->defs + TAILLE_SAISIE, TAILLE_CALCULEE);
+#else
+  reset_tab(irdata->calculee, irdata->def_calculee, TAILLE_CALCULEE);
+#endif /* FLG_COMPACT */
+}
+
+void IRDATA_recopie_irdata(irdata_src, irdata_dst)
+T_irdata *irdata_src ;
+T_irdata *irdata_dst ;
+{
+memcpy(irdata_dst->saisie, irdata_src->saisie, TAILLE_SAISIE * sizeof(double)) ;
+memcpy(irdata_dst->def_saisie, irdata_src->def_saisie, TAILLE_SAISIE) ;
+memcpy(irdata_dst->calculee, irdata_src->calculee, TAILLE_CALCULEE * sizeof(double)) ;
+memcpy(irdata_dst->def_calculee, irdata_src->def_calculee, TAILLE_CALCULEE) ;
+memcpy(irdata_dst->base, irdata_src->base, TAILLE_BASE * sizeof(double)) ;
+memcpy(irdata_dst->def_base, irdata_src->def_base, TAILLE_BASE) ;
+}
+
+void IRDATA_reset_erreur(T_irdata *irdata)
+{
+#ifdef FLG_MULTITHREAD
+  *irdata->p_discord = irdata->tas_discord;
+  irdata->tas_discord = irdata->discords;
+  irdata->discords = 0;
+  irdata->p_discord = &irdata->discords;
+  irdata->nb_bloquantes = 0;
+#endif /* FLG_MULTITHREAD */
+}
+
+T_discord * IRDATA_range(T_irdata *irdata, T_desc_var *desc, double valeur)
+{
+  int indice = 0;
+  T_discord *discord = NULL;
+  if (valeur < 0) {
+    discord = err_NEGATIF(irdata);
+  } else {
+    discord = (*desc->verif)(irdata);
+  }
+  if ((discord != NULL) && (discord->erreur->type == ANOMALIE)) {
+    return discord;
+  }
+#ifdef FLG_COMPACT
+  indice = desc->indice;
+  irdata->valeurs[indice] = valeur;
+  irdata->defs[indice] = 1;
+#else
+  indice = desc->indice & INDICE_VAL;
+  if ((desc->indice & EST_MASQUE) != EST_SAISIE) {
+    return (0);
+  }
+  irdata->saisie[indice] = valeur;
+  irdata->def_saisie[indice] = 1;
+#endif /* FLG_COMPACT */
+  return discord;
+}
+
+void IRDATA_range_base(T_irdata *irdata, T_desc_var *desc, double valeur)
+{
+#ifdef FLG_COMPACT
+  int indice = desc->indice;
+  irdata->defs[indice] = 1;
+  irdata->valeurs[indice] = valeur;
+#else
+  int indice = desc->indice & INDICE_VAL;
+  switch (desc->indice & EST_MASQUE) {
+    case EST_SAISIE:
+      irdata->def_saisie[indice] = 1;
+      irdata->saisie[indice] = valeur;
+      break;
+    case EST_CALCULEE:
+      irdata->def_calculee[indice] = 1;
+      irdata->calculee[indice] = valeur;
+      break;
+    case EST_BASE:
+      irdata->def_base[indice] = 1;
+      irdata->base[indice] = valeur;
+      break;
+  }
+#endif /* FLG_COMPACT */
+}
+
+struct S_discord * IRDATA_range_tableau(T_irdata *irdata, T_desc_var *desc, int ind, double valeur)
+{
+  int indice = 0;
+  T_discord *discord = NULL;
+  discord = (*desc->verif)(irdata);
+  if ((discord != NULL) && (discord->erreur->type == ANOMALIE)) {
+    return discord;
+  }
+#ifdef FLG_COMPACT
+  indice = desc->indice + ind;
+  irdata->defs[indice] = 1;
+  irdata->valeurs[indice] = valeur;
+#else
+  indice = (desc->indice & INDICE_VAL) + ind;
+  switch (desc->indice & EST_MASQUE) {
+    case EST_SAISIE:
+      irdata->def_saisie[indice] = 1;
+      irdata->saisie[indice] = valeur;
+      break;
+    case EST_CALCULEE:
+      irdata->def_calculee[indice] = 1;
+      irdata->calculee[indice] = valeur;
+      break;
+    case EST_BASE:
+      irdata->def_base[indice] = 1;
+      irdata->base[indice] = valeur;
+      break;
+  }
+#endif /* FLG_COMPACT */
+  return discord;
+}
+
+double * IRDATA_extrait_special(T_irdata *irdata, T_desc_var *desc)
+{
+  double *res = NULL;
+#ifdef FLG_COMPACT
+  int indice = desc->indice;
+  res = (irdata->defs[indice] == 0) ? NULL : &irdata->valeurs[indice];
+#else
+  int indice = desc->indice & INDICE_VAL;
+  switch (desc->indice & EST_MASQUE) {
+    case EST_SAISIE:
+      res = (irdata->def_saisie[indice] == 0) ? NULL : &irdata->saisie[indice];
+      break;
+    case EST_CALCULEE:
+      res = (irdata->def_calculee[indice] == 0) ? NULL : &irdata->calculee[indice];
+      break;
+    case EST_BASE:
+      res = (irdata->def_base[indice] == 0) ? NULL : &irdata->base[indice];
+      break;
+    default:
+      res = NULL;
+      break;
+  }
+#endif /* FLG_COMPACT */
+  return res;
+}
+
+double * IRDATA_extrait_tableau(T_irdata *irdata, T_desc_var *desc, int ind)
+{
+  double *res = NULL;
+#ifdef FLG_COMPACT
+  int indice = desc->indice + ind;
+  res = (irdata->defs[indice] == 0) ? NULL : &irdata->valeurs[indice];
+#else
+  int indice = (desc->indice & INDICE_VAL) + ind;
+  switch (desc->indice & EST_MASQUE) {
+    case EST_SAISIE:
+      res = (irdata->def_saisie[indice] == 0) ? NULL : &irdata->saisie[indice];
+      break;
+    case EST_CALCULEE:
+      res = (irdata->def_calculee[indice] == 0) ? NULL : &irdata->calculee[indice];
+      break;
+    case EST_BASE:
+      res = (irdata->def_base[indice] == 0) ? NULL : &irdata->base[indice];
+      break;
+    default:
+      res = NULL;
+      break;
+  }
+#endif /* FLG_COMPACT */
+  return res;
+}
+
+static T_desc_var * cherche_desc_var(const char *nom, T_desc_var *table, int taille, int sup)
+{
+  T_desc_var *desc = NULL;
+  int res = -1, inf = 0, millieu;
+  while ((res != 0) && (inf < sup)) {
+    millieu = (inf + sup) / 2;
+    desc = (T_desc_var *)((char *)table + (taille * millieu));
+    res = strcmp(nom, desc->nom);
+    if (res < 0) sup = millieu;
+    else if (res > 0) inf = millieu + 1;
+  }
+  if (res == 0)
+    return desc;
+  else
+    return NULL;
+}
+
+T_desc_var * IRDATA_cherche_desc_var(const char *nom)
+{
+  static T_desc_var * desc[6] = {
+    (T_desc_var *)desc_contexte, (T_desc_var *)desc_famille, (T_desc_var *)desc_revenu,
+    (T_desc_var *)desc_revenu_correc, (T_desc_var *)desc_variation, (T_desc_var *)desc_restituee };
+  static const int size[6] = {
+    sizeof(T_desc_contexte), sizeof(T_desc_famille), sizeof(T_desc_revenu),
+    sizeof(T_desc_revenu_correc), sizeof(T_desc_variation), sizeof(T_desc_restituee) };
+  static const int nb[6] = {
+    NB_CONTEXTE, NB_FAMILLE, NB_REVENU,
+    NB_REVENU_CORREC, NB_VARIATION, NB_RESTITUEE };
+  int i = 0;
+  for (i = 0; i < 6; ++i) {
+    T_desc_var *res = cherche_desc_var(nom, desc[i], size[i], nb[i]);
+    if (res != NULL) return res;
+  }
+  return NULL;
+}
