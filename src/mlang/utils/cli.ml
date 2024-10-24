c95eb43db0351ef9cc09faf8850a89c26b0d3bb7
David MICHEL <david.michel1@dgfip.finances.gouv.fr>
2024-10-24 15:31:29 +0200

    Application iliad par défaut

--- src/mlang/utils/cli.ml
+++ src/mlang/utils/cli.ml
@@ -30,8 +30,7 @@ let files =
     & info [] ~docv:"FILES" ~doc:"M files to be compiled")
 
 let applications =
-  Arg.(
-    non_empty & opt (list string) [] & info [ "A" ] ~doc:"Application name(s)")
+  Arg.(value & opt (list string) [] & info [ "A" ] ~doc:"Application name(s)")
 
 let without_dgfip_m =
   Arg.(
@@ -288,7 +287,7 @@ let set_all_arg_refs (files_ : string list) applications_
     (income_year_ : int option) (value_sort_ : value_sort)
     (round_ops_ : round_ops) =
   source_files := files_;
-  application_names := applications_;
+  (application_names := match applications_ with [] -> [ "iliad" ] | al -> al);
   without_dgfip_m := without_dgfip_m_;
   debug_flag := debug_;
   var_info_debug := var_info_debug_;
