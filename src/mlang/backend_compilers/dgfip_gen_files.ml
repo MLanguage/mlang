9cfa755d3207a3c59ed8891a04ec2f88f300e607
David MICHEL <david.michel1@dgfip.finances.gouv.fr>
2026-05-19 16:11:47 +0200

    Traces pour les cellulles des tableaux dans les règles

--- src/mlang/backend_compilers/dgfip_gen_files.ml
+++ src/mlang/backend_compilers/dgfip_gen_files.ml
@@ -375,11 +375,18 @@ let gen_dbg fmt =
     {|int change_couleur(int couleur, int typographie);
 int get_couleur(void);
 int get_typo(void);
-    
+
 #ifdef FLG_TRACE
+
+#ifdef FLG_API
+#define TRACE_FILE fd_trace_dialog
+#else
+#define TRACE_FILE stderr
+#endif /* FLG_API */
+
 extern int niv_trace;
 
-extern void aff1(char *nom);
+extern void aff1(const char *nom);
 
 extern void aff_val(const char *nom, const T_irdata *irdata, int indice, int niv, const char *chaine, int is_tab, int expr, int maxi);
 
@@ -730,21 +737,27 @@ extern int nb_informatives(T_irdata *irdata);
 extern int nb_discordances(T_irdata *irdata);
 extern int nb_anomalies(T_irdata *irdata);
 extern int nb_bloquantes(T_irdata *irdata);
-extern void nettoie_erreur _PROTS((T_irdata *irdata ));
-extern void finalise_erreur _PROTS((T_irdata *irdata ));
-extern void nettoie_erreurs_finalisees _PROTS((T_irdata *irdata ));
-extern void exporte_erreur _PROTS((T_irdata *irdata ));
+extern void nettoie_erreur (T_irdata *irdata);
+extern void finalise_erreur (T_irdata *irdata);
+extern void nettoie_erreurs_finalisees (T_irdata *irdata);
+extern void exporte_erreur (T_irdata *irdata);
 
 extern T_irdata *cree_irdata(void);
-extern void init_saisie(T_irdata *irdata, int sp);
-extern void init_calculee(T_irdata *irdata, int sp);
-extern void init_base(T_irdata *irdata, int sp);
+extern void init_saisie_spc(T_irdata *irdata, int sp);
+extern void init_calculee_spc(T_irdata *irdata, int sp);
+extern void init_base_spc(T_irdata *irdata, int sp);
+extern void init_saisie(T_irdata *irdata);
+extern void init_calculee(T_irdata *irdata);
+extern void init_base(T_irdata *irdata);
 extern void init_erreur(T_irdata *irdata);
 extern void detruis_irdata(T_irdata *irdata);
 extern void set_max_bloquantes(T_irdata *irdata, const int max_ano);
-extern void recopie_saisie(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
-extern void recopie_calculee(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
-extern void recopie_base(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
+extern void recopie_saisie_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
+extern void recopie_calculee_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
+extern void recopie_base_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst);
+extern void recopie_saisie(T_irdata *irdata_src, T_irdata *irdata_dst);
+extern void recopie_calculee(T_irdata *irdata_src, T_irdata *irdata_dst);
+extern void recopie_base(T_irdata *irdata_src, T_irdata *irdata_dst);
 extern void ecris_saisie(T_irdata *irdata, int idx, char def, double val);
 extern void ecris_calculee(T_irdata *irdata, int idx, char def, double val);
 extern void ecris_base(T_irdata *irdata, int idx, char def, double val);
@@ -1311,23 +1324,9 @@ static void init_tab(char *p_def, double *p_val, int nb) {
   memset(p_def, 0, nb);
 }
 
-void init_saisie(T_irdata *irdata, int sp) {
-  if (irdata == NULL) return;
-  if (sp < 0 || NB_ESPACES_VARIABLES <= sp) sp = ESPACE_PAR_DEFAUT;
-  if (irdata->var_spaces[sp].saisie == NULL) return;
-  init_tab(irdata->var_spaces[sp].def_saisie, irdata->var_spaces[sp].saisie, TAILLE_SAISIE);
-}
-
 void init_saisie_espace(char *def, double *val) {
   if (def == NULL || val == NULL) return;
   init_tab(def, val, TAILLE_SAISIE);
-}
-
-void init_calculee(T_irdata *irdata, int sp) {
-  if (irdata == NULL) return;
-  if (sp < 0 || NB_ESPACES_VARIABLES <= sp) sp = ESPACE_PAR_DEFAUT;
-  if (irdata->var_spaces[sp].calculee == NULL) return;
-  init_tab(irdata->var_spaces[sp].def_calculee, irdata->var_spaces[sp].calculee, TAILLE_CALCULEE);
 }
 
 void init_calculee_espace(char *def, double *val) {
@@ -1335,16 +1334,42 @@ void init_calculee_espace(char *def, double *val) {
   init_tab(def, val, TAILLE_CALCULEE);
 }
 
-void init_base(T_irdata *irdata, int sp) {
+void init_base_espace(char *def, double *val) {
+  if (def == NULL || val == NULL) return;
+  init_tab(def, val, TAILLE_BASE);
+}
+
+void init_saisie_spc(T_irdata *irdata, int sp) {
+  if (irdata == NULL) return;
+  if (sp < 0 || NB_ESPACES_VARIABLES <= sp) sp = ESPACE_PAR_DEFAUT;
+  if (irdata->var_spaces[sp].saisie == NULL) return;
+  init_tab(irdata->var_spaces[sp].def_saisie, irdata->var_spaces[sp].saisie, TAILLE_SAISIE);
+}
+
+void init_calculee_spc(T_irdata *irdata, int sp) {
+  if (irdata == NULL) return;
+  if (sp < 0 || NB_ESPACES_VARIABLES <= sp) sp = ESPACE_PAR_DEFAUT;
+  if (irdata->var_spaces[sp].calculee == NULL) return;
+  init_tab(irdata->var_spaces[sp].def_calculee, irdata->var_spaces[sp].calculee, TAILLE_CALCULEE);
+}
+
+void init_base_spc(T_irdata *irdata, int sp) {
   if (irdata == NULL) return;
   if (sp < 0 || NB_ESPACES_VARIABLES <= sp) sp = ESPACE_PAR_DEFAUT;
   if (irdata->var_spaces[sp].base == NULL) return;
   init_tab(irdata->var_spaces[sp].def_base, irdata->var_spaces[sp].base, TAILLE_BASE);
 }
 
-void init_base_espace(char *def, double *val) {
-  if (def == NULL || val == NULL) return;
-  init_tab(def, val, TAILLE_BASE);
+void init_saisie(T_irdata *irdata) {
+  init_saisie_spc(irdata, ESPACE_PAR_DEFAUT);
+}
+
+void init_calculee(T_irdata *irdata) {
+  init_calculee_spc(irdata, ESPACE_PAR_DEFAUT);
+}
+
+void init_base(T_irdata *irdata) {
+  init_base_spc(irdata, ESPACE_PAR_DEFAUT);
 }
 
 void init_erreur(T_irdata *irdata) {
@@ -1533,7 +1558,7 @@ void set_max_bloquantes(T_irdata *irdata, const int max_ano) {
   }
 }
 
-void recopie_saisie(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
+void recopie_saisie_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
   if (irdata_src == NULL || irdata_dst == NULL) return;
   if (0 < sp_src || sp_src <= NB_ESPACES_VARIABLES) sp_src = ESPACE_PAR_DEFAUT;
   if (0 < sp_dst || sp_dst <= NB_ESPACES_VARIABLES) sp_dst = ESPACE_PAR_DEFAUT;
@@ -1552,7 +1577,7 @@ void recopie_saisie(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int 
   );
 }
 
-void recopie_calculee(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
+void recopie_calculee_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
   if (irdata_src == NULL || irdata_dst == NULL) return;
   if (0 < sp_src || sp_src <= NB_ESPACES_VARIABLES) sp_src = ESPACE_PAR_DEFAUT;
   if (0 < sp_dst || sp_dst <= NB_ESPACES_VARIABLES) sp_dst = ESPACE_PAR_DEFAUT;
@@ -1571,7 +1596,7 @@ void recopie_calculee(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, in
   );
 }
 
-void recopie_base(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
+void recopie_base_spc(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp_dst) {
   if (irdata_src == NULL || irdata_dst == NULL) return;
   if (0 < sp_src || sp_src <= NB_ESPACES_VARIABLES) sp_src = ESPACE_PAR_DEFAUT;
   if (0 < sp_dst || sp_dst <= NB_ESPACES_VARIABLES) sp_dst = ESPACE_PAR_DEFAUT;
@@ -1588,6 +1613,18 @@ void recopie_base(T_irdata *irdata_src, int sp_src, T_irdata *irdata_dst, int sp
     irdata_src->var_spaces[sp_src].def_base,
     TAILLE_BASE
   );
+}
+
+void recopie_saisie(T_irdata *irdata_src, T_irdata *irdata_dst) {
+  recopie_saisie_spc(irdata_src, ESPACE_PAR_DEFAUT, irdata_dst, ESPACE_PAR_DEFAUT);
+}
+
+void recopie_calculee(T_irdata *irdata_src, T_irdata *irdata_dst) {
+  recopie_calculee_spc(irdata_src, ESPACE_PAR_DEFAUT, irdata_dst, ESPACE_PAR_DEFAUT);
+}
+
+void recopie_base(T_irdata *irdata_src, T_irdata *irdata_dst) {
+  recopie_base_spc(irdata_src, ESPACE_PAR_DEFAUT, irdata_dst, ESPACE_PAR_DEFAUT);
 }
 
 static void ecris_tab(char *t_def, double *t_val, int t_nb, int idx, char def, double val) {
@@ -2065,12 +2102,99 @@ double *lis_tabaccess_val_ptr(
   return lis_varinfo_val_ptr(irdata, var_space, info);
 }
 
+void trace_tabaccess(
+  const char *chaine, int niv,
+  T_irdata *irdata, int var_space, int idx_tab,
+  char idx_def, double idx_val
+) {
+#ifdef FLG_TRACE
+  T_varinfo *info = NULL;
+  int idx = (int)idx_val;
+  char res_def = 0;
+  double res_val = 0.0;
+  char *nom = NULL;
+
+  if (irdata == NULL || idx_tab < 0 || TAILLE_TAB_VARINFO <= idx_tab) return;
+  info = tab_varinfo[idx_tab];
+  nom = info->name;
+
+  if (idx_def == 0) {
+    if (niv_trace >= niv) {
+#ifdef FLG_COLORS
+      fprintf(TRACE_FILE, "\033[%d;%dm%s[undef] %s 0\033[0m\n",
+        color, typo, nom, chaine);
+#else
+      fprintf(TRACE_FILE, "%s[undef] %s 0m\n", nom, chaine);
+#endif /* FLG_COLORS */
+    }
+    return;
+  } else if (idx < 0) {
+    if (niv_trace >= niv) {
+#ifdef FLG_COLORS
+      fprintf(TRACE_FILE, "\033[%d;%dm%s[%d] %s 0\033[0m\n",
+        color, typo, nom, idx, chaine);
+#else
+      fprintf(TRACE_FILE, "%s[%d] %s 0m\n", nom, idx, chaine);
+#endif /* FLG_COLORS */
+    }
+    return;
+  } else if (idx >= info->size) {
+#ifdef FLG_COLORS
+    fprintf(TRACE_FILE,
+      "\033[%d;%dmerreur: indice (%d) superieur au maximum (%d)\033[0m\n",
+      color, typo, idx, info->size);
+#else
+    fprintf(TRACE_FILE, "erreur: indice (%d) superieur au maximum (%d)\n",
+      idx, info->size);
+#endif /* FLG_COLORS */
+    idx = 0;
+  }
+
+  info = tab_varinfo[idx_tab + idx + 1];
+  lis_varinfo(irdata, var_space, info, &res_def, &res_val);
+  if (res_def == 0) {
+    if (res_val != 0.0) {
+#ifdef FLG_COLORS
+      fprintf(TRACE_FILE, "\033[%d;%dm%s[%d] : erreur undef = %lf\033[0m\n",
+        color, typo, nom, idx, res_val);
+#else
+      fprintf(TRACE_FILE, "%s[%d] : erreur undef = %lf\n", nom, idx, res_val);
+#endif /* FLG_COLORS */
+    } else if (niv_trace >= niv) {
+#ifdef FLG_COLORS
+      fprintf(TRACE_FILE, "\033[%d;%dm%s[%d] %s undef\033[0m\n",
+              color, typo, nom, idx, chaine);
+#else
+      fprintf(TRACE_FILE, "%s[%d] %s undef\n", nom, idx, chaine);
+#endif /* FLG_COLORS */
+    }
+  } else if (res_def != 1) {
+#ifdef FLG_COLORS
+    fprintf(TRACE_FILE, "\033[%d;%dm%s[%d] : erreur flag def = %d\033[0m\n",
+            color, typo, nom, idx, res_def);
+#else
+    fprintf(TRACE_FILE, "%s[%d] : erreur flag def = %d\n", nom, idx, res_def);
+#endif /* FLG_COLORS */
+  } else if (niv_trace >= niv) {
+#ifdef FLG_COLORS
+    fprintf(TRACE_FILE, "\033[%d;%dm%s[%d] %s %lf\033[0m\n",
+            color, typo, nom, idx, chaine, res_val);
+#else
+    fprintf(TRACE_FILE, "%s[%d] %s %lf\n", nom, idx, chaine, res_val);
+#endif /* FLG_COLORS */
+  }
+#endif /* FLG_TRACE */
+}
+
 char lis_tabaccess(
   T_irdata *irdata, int var_space, int idx_tab,
   char idx_def, double idx_val,
   char *res_def, double *res_val
 ) {
   T_varinfo *info = lis_tabaccess_varinfo(irdata, idx_tab, idx_def, idx_val);
+#ifdef FLG_TRACE
+  trace_tabaccess(":", 3, irdata, var_space, idx_tab, idx_def, idx_val);
+#endif /* FLG_TRACE */
   int idx = 0;
   if (info == NULL) {
     *res_val = 0.0;
@@ -2106,6 +2230,9 @@ void ecris_tabaccess(
 ) {
   T_varinfo *info = lis_tabaccess_varinfo(irdata, idx_tab, idx_def, idx_val);
   ecris_varinfo(irdata, var_space, info, def, val);
+#ifdef FLG_TRACE
+  trace_tabaccess("<-", 2, irdata, var_space, idx_tab, idx_def, idx_val);
+#endif /* FLG_TRACE */
   /* tableau originel */
   /*ecris_varinfo_tab(irdata, var_space, idx_tab, idx_def, idx_val, def, val);*/
 }
@@ -2205,15 +2332,7 @@ char est_type(T_varinfo *info, int type, char *res_def, double *res_val) {
 
 /* int niv_trace = 3; */
 
-#ifdef FLG_API
-#define TRACE_FILE fd_trace_dialog
-#else
-#define TRACE_FILE stderr
-#endif /* FLG_API */
-
-void aff1(nom)
-char *nom ;
-{
+void aff1(const char *nom) {
 #ifdef FLG_COLORS
 if (niv_trace >= 1) fprintf(stderr, "\033[%d;%dm%s\033[0m", color, typo, nom) ;
 #else
